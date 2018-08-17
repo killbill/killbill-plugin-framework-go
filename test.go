5f1dd2f84ad6a6dfa14d42b63a4e971959812303
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-08-17 11:01:06 -0700

    test: rename helpers
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- test.go
+++ test.go
@@ -8,8 +8,8 @@ import (
 	"reflect"
 )
 
-// assert fails the test if the condition is false.
-func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+// Assert fails the test if the condition is false.
+func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
@@ -17,8 +17,8 @@ func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	}
 }
 
-// ok fails the test if an err is not nil.
-func ok(tb testing.TB, err error) {
+// AssertOk fails the test if an err is not nil.
+func AssertOk(tb testing.TB, err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
@@ -26,8 +26,8 @@ func ok(tb testing.TB, err error) {
 	}
 }
 
-// equals fails the test if exp is not equal to act.
-func equals(tb testing.TB, exp, act interface{}) {
+// AssertEquals fails the test if exp is not equal to act.
+func AssertEquals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
